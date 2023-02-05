1adba3704085764c432c9b34477f1c5ec449138b
Chemaclass <chemaclass@outlook.es>
2023-02-05 17:40:25 +0100

    Add tests for success and error without amount

--- tests/LnAddressTest.php
+++ tests/LnAddressTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 final class LnAddressTest extends TestCase
 {
-    public function test_successful_payment_request(): void
+    public function test_successful_payment_request_with_amount(): void
     {
         $backend = 'lnbits';
         $backend_options = array();
@@ -37,7 +37,34 @@ final class LnAddressTest extends TestCase
         );
     }
 
-    public function test_error_payment_request(): void
+    public function test_successful_payment_request_without_amount(): void
+    {
+        $backend = 'lnbits';
+        $backend_options = array();
+        $backend_options['lnbits'] = [
+            'api_endpoint' => 'http://localhost:5000',
+            'api_key' => ''
+        ];
+
+        $_SERVER['HTTP_HOST'] = 'localhost';
+        $_SERVER['REQUEST_URI'] = '/ping';
+        $_GET['amount'] = 0;
+
+        $httpApi = $this->createStub(HttpApiInterface::class);
+        $httpApi->method('get')->willReturn(
+            json_encode([
+                'payment_request' => 'any payment_request',
+            ])
+        );
+        $lnAddress = new LnAddress($httpApi);
+        $lnAddress->generateInvoice($backend, $backend_options);
+
+        self::expectOutputString(
+            '{"callback":"https://localhost/ping","maxSendable":10000000000,"minSendable":100000,"metadata":"[[\"text/plain\",\"Pay to LnAddress@localhost\"],[\"text/identifier\",\"LnAddress@localhost\"]]","tag":"payRequest","commentAllowed":0}'
+        );
+    }
+
+    public function test_error_payment_request_with_amount(): void
     {
         $backend = 'lnbits';
         $backend_options = array();
@@ -57,6 +84,29 @@ final class LnAddressTest extends TestCase
 
         self::expectOutputString(
             '{"status":"ERROR","reason":"Backend is unreachable"}'
+        );
+    }
+
+    public function test_error_payment_request_without_amount(): void
+    {
+        $backend = 'lnbits';
+        $backend_options = array();
+        $backend_options['lnbits'] = [
+            'api_endpoint' => 'http://localhost:5000',
+            'api_key' => ''
+        ];
+
+        $_SERVER['HTTP_HOST'] = 'localhost';
+        $_SERVER['REQUEST_URI'] = '/ping';
+        $_GET['amount'] = 0;
+
+        $httpApi = $this->createStub(HttpApiInterface::class);
+        $httpApi->method('get')->willReturn(null);
+        $lnAddress = new LnAddress($httpApi);
+        $lnAddress->generateInvoice($backend, $backend_options);
+
+        self::expectOutputString(
+            '{"callback":"https://localhost/ping","maxSendable":10000000000,"minSendable":100000,"metadata":"[[\"text/plain\",\"Pay to LnAddress@localhost\"],[\"text/identifier\",\"LnAddress@localhost\"]]","tag":"payRequest","commentAllowed":0}'
         );
     }
 }
