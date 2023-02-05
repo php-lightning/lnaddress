874183ee03480e9554a433137d84327233b03f9b
Chemaclass <chemaclass@outlook.es>
2023-02-05 20:36:28 +0100

    Refactor LnAddress

--- src/LnAddress.php
+++ src/LnAddress.php
@@ -9,7 +9,10 @@ use function strlen;
 final class LnAddress
 {
     public const MESSAGE_PAYMENT_RECEIVED = 'Payment received!';
+
     private const DEFAULT_BACKEND = 'lnbits';
+
+    private const TAG_PAY_REQUEST = 'payRequest';
 
     /** @var int 100 Minimum in msat (sat/1000) */
     private const DEFAULT_MIN_SENDABLE = 100_000;
@@ -33,83 +36,58 @@ final class LnAddress
      *     api_key?: string,
      *   }
      * } $backendOptions
+     * @param string $image_file The picture you want to display, if you don't want to show a picture, leave an empty string.
+     *                          Beware that a heavy picture will make the wallet fails to execute lightning address process! 136536 bytes maximum for base64 encoded picture data
      */
     public function generateInvoice(
         int $amount,
         string $backend = self::DEFAULT_BACKEND,
         array $backendOptions = [],
+        string $image_file = '',
     ): array {
         // automatically define the ln address based on filename & host, this shouldn't be changed
         $username = str_replace('.php', '', basename(__FILE__));
-        $ln_address = $username . '@' . $this->config->getHttpHost();
+        $lnAddress = $username . '@' . $this->config->getHttpHost();
 
         // Modify the description if you want to custom it
         // This will be the description on the wallet that pays your ln address
-        $description = 'Pay to ' . $ln_address;
+        $description = 'Pay to ' . $lnAddress;
 
-        // Success payment message, this is the confirmation message that the person who paid will see once your ln address has received sats
-        $minSendable = self::DEFAULT_MIN_SENDABLE;
-        $maxSendable = self::DEFAULT_MAX_SENDABLE;
-
-        // Modify the following line with the path to the picture you want to display, if you don't want to show a picture, leave an empty string
-        // Beware that a heavy picture will make the wallet fails to execute lightning address process! 136536 bytes maximum for base64 encoded picture data
-        $image_file = '';
-
-        // From this line, except if you know what you're doing, you don't need to change anything.
-
-        // Comment feature not yet implemented, future use
-        $allow_comment = false;
-        $max_comment_length = 0;
-
-        // requestinvoice($backend, $backend_options, $amount, $metadata, $lnaddr, $comment_allowed, $comment)
-        // This function handles flows with the backend
-
-        if (!empty($image_file)) {
-            $img_metadata = ',["image/jpeg;base64","' . base64_encode($this->httpApi->get($image_file)) . '"]';
-        } else {
-            $img_metadata = '';
-        }
-
-        $metadata = '[["text/plain","' . $description . '"],["text/identifier","' . $ln_address . '"]' . $img_metadata . ']';
+        $imageMetadata = $this->generateImageMetadata($image_file);
+        $metadata = '[["text/plain","' . $description . '"],["text/identifier","' . $lnAddress . '"]' . $imageMetadata . ']';
 
         // payRequest json data, spec : https://github.com/lnurl/luds/blob/luds/06.md
         $data = [
-            'callback' => 'https://' . $this->config->getHttpHost() . $this->config->getRequestUri(),
-            'maxSendable' => $maxSendable,
-            'minSendable' => $minSendable,
+            'callback' => $this->callback(),
+            'maxSendable' => self::DEFAULT_MAX_SENDABLE,
+            'minSendable' => self::DEFAULT_MIN_SENDABLE,
             'metadata' => $metadata,
-            'tag' => 'payRequest',
-            'commentAllowed' => $allow_comment ? $max_comment_length : 0,
+            'tag' => self::TAG_PAY_REQUEST,
+            'commentAllowed' => 0, // TODO: Not implemented yet
         ];
 
         if ($amount === 0) {
             return $data;
         }
 
-
-        if ($amount < $minSendable || $amount > $maxSendable) {
+        if (!$this->isValidAmount($amount)) {
             return [
                 'status' => 'ERROR',
                 'reason' => 'Amount is not between minimum and maximum sendable amount',
             ];
         }
 
-        $resp_payload = [];
-        $backend_data = json_decode(
-            $this->requestInvoice(
-                $backend,
-                $backendOptions[$backend] ?? [],
-                $amount / 1000,
-                $metadata,
-                $ln_address,
-            ),
-            true,
-            512,
-            JSON_THROW_ON_ERROR,
+        $invoice = $this->requestInvoice(
+            $backend,
+            $backendOptions[$backend] ?? [],
+            $amount / 1000,
+            $metadata,
+            $lnAddress,
         );
-        if ($backend_data['status'] === 'OK') {
-            $resp_payload = [
-                'pr' => $backend_data['pr'],
+
+        if ($invoice['status'] === 'OK') {
+            return [
+                'pr' => $invoice['pr'],
                 'status' => 'OK',
                 'successAction' => [
                     'tag' => 'message',
@@ -118,23 +96,23 @@ final class LnAddress
                 'routes' => [],
                 'disposable' => false,
             ];
-        } else {
-            $resp_payload['status'] = $backend_data['status'];
-            $resp_payload['reason'] = $backend_data['reason'];
         }
 
-        return $resp_payload;
+        return [
+            'status' => $invoice['status'],
+            'reason' => $invoice['reason'],
+        ];
     }
 
     private function requestInvoice(
-        $backend,
-        $backend_options,
-        $amount,
-        $metadata,
-        $lnaddr,
-        $comment_allowed = false,
-        $comment = null,
-    ): string {
+        string $backend,
+        array $backend_options,
+        float $amount,
+        string $metadata,
+        string $lnaddr = '',
+        bool $comment_allowed = false,
+        ?string $comment = null,
+    ): array {
         if ($backend === self::DEFAULT_BACKEND) {
             $http_method = 'POST';
             $api_route = '/api/v1/payments';
@@ -157,20 +135,41 @@ final class LnAddress
             ];
 
             $req_context = stream_context_create($http_req);
-            $req_result = $this->httpApi->get($backend_options['api_endpoint'] . $api_route, $req_context);
+            $response = $this->httpApi->get($backend_options['api_endpoint'] . $api_route, $req_context);
 
-            if ($req_result !== null) {
-                return json_encode(
-                    [
-                        'status' => 'OK',
-                        'pr' => json_decode($req_result, true, 512, JSON_THROW_ON_ERROR)['payment_request'],
-                    ],
-                    JSON_THROW_ON_ERROR,
-                );
+            if ($response !== null) {
+                $responseJson = json_decode($response, true, 512, JSON_THROW_ON_ERROR);
+
+                return [
+                    'status' => 'OK',
+                    'pr' => $responseJson['payment_request'],
+                ];
             }
         }
 
-        // backend handled
-        return json_encode(['status' => 'ERROR', 'reason' => 'Backend is unreachable'], JSON_THROW_ON_ERROR);
+        return [
+            'status' => 'ERROR',
+            'reason' => 'Backend is unreachable',
+        ];
+    }
+
+    private function callback(): string
+    {
+        return 'https://' . $this->config->getHttpHost() . $this->config->getRequestUri();
+    }
+
+    private function generateImageMetadata(string $imageFile): string
+    {
+        if ($imageFile === '') {
+            return '';
+        }
+
+        return ',["image/jpeg;base64","' . base64_encode($this->httpApi->get($imageFile)) . '"]';
+    }
+
+    private function isValidAmount(int $amount): bool
+    {
+        return $amount >= self::DEFAULT_MIN_SENDABLE
+            && $amount <= self::DEFAULT_MAX_SENDABLE;
     }
 }
