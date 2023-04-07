03d42d8fb2aad3617c65e4ee7ed8fb42d592e3f9
Chemaclass <chemaclass@outlook.es>
2023-04-07 18:40:04 +0200

    Create InvoiceController using Symfony

--- public/index.php
+++ public/index.php
@@ -1,34 +1,9 @@
 <?php
 
-declare(strict_types=1);
+use PhpLightning\Kernel;
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-use Gacela\Framework\Gacela;
-use PhpLightning\Lightning;
-
-$cwd = (string)getcwd();
-if (!file_exists($autoloadPath = $cwd . '/vendor/autoload.php')) {
-    exit("Cannot load composer's autoload file: " . $autoloadPath);
-}
-
-require $autoloadPath;
-
-Gacela::bootstrap($cwd, Lightning::configFn());
-
-// For now lnbits is the only backend supported
-$backend = 'lnbits';
-
-$milliSats = (int)($argv[1] ?? $_GET['amount'] ?? 0);
-
-try {
-    if ($milliSats === 0) {
-        echo Lightning::getCallbackUrl();
-    } else {
-        echo Lightning::generateInvoice($milliSats, $backend);
-    }
-} catch (Throwable $e) {
-    echo $e->getMessage();
-}
-echo PHP_EOL;
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
