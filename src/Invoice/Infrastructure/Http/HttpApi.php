9df4d0132f8bf021c5f966bfbaadedb25d8f7014
Chemaclass <chemaclass@outlook.es>
2023-04-07 22:31:48 +0200

    Install and use Symfony HttpClient

--- src/Invoice/Infrastructure/Http/HttpApi.php
+++ src/Invoice/Infrastructure/Http/HttpApi.php
@@ -5,33 +5,18 @@ declare(strict_types=1);
 namespace PhpLightning\Invoice\Infrastructure\Http;
 
 use PhpLightning\Invoice\Domain\Http\HttpApiInterface;
+use Symfony\Component\HttpClient\HttpClient;
 
 final class HttpApi implements HttpApiInterface
 {
     public function postRequestInvoice(string $uri, string $body, array $headers = []): ?array
     {
-        // TODO: Consider using something like this:
-        //        HttpClient::create()
-        //            ->request('POST', $url, $options)
-        //            ->getContent();
-        $options = [
-            explode(':', $uri)[0] => [ // extract protocol from $uri
-                'header' => implode(
-                    "\r\n",
-                    array_map(
-                        static fn (string $v, string $k) => sprintf('%s: %s', $k, $v),
-                        $headers,
-                        array_keys($headers),
-                    ),
-                ),
-                'method' => 'POST',
-                'content' => $body,
-            ],
-        ];
+        $response = HttpClient::create()
+            ->request('POST', $uri, [
+                'headers' => $headers,
+                'body' => $body,
+            ]);
 
-        $context = stream_context_create($options);
-        $response = (string)file_get_contents($uri, false, $context);
-
-        return json_decode($response, true, 512, JSON_THROW_ON_ERROR);
+        return json_decode($response->getContent(), true, 512, JSON_THROW_ON_ERROR);
     }
 }
