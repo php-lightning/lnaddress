e8d809dba277b85a7cf073f5a97884a73549d4a5
Chemaclass <chemaclass@outlook.es>
2023-04-16 18:57:27 +0200

    Update router

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 use Gacela\Framework\Gacela;
-use Gacela\Router\Route;
+use Gacela\Router\Routing;
 use Gacela\Router\RoutingConfigurator;
 use PhpLightning\Invoice\Infrastructure\Controller\InvoiceController;
 use PhpLightning\Kernel;
@@ -15,7 +15,7 @@ require_once getcwd() . '/vendor/autoload.php';
 
 Gacela::bootstrap(getcwd(), Kernel::gacelaConfigFn());
 
-Route::configure(static function (RoutingConfigurator $routes): void {
+Routing::configure(static function (RoutingConfigurator $routes): void {
     $routes->get('{username}/{amount}', InvoiceController::class);
     $routes->get('{username}', InvoiceController::class);
     $routes->get('/', InvoiceController::class);
