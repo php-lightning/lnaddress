2eea92d80c66d10e9475d116596edea6b3cbc861
Chemaclass <chemaclass@outlook.es>
2023-04-27 13:12:47 +0200

    add nostr.dist.json

--- lightning-config.dist.php
+++ lightning-config.dist.php
@@ -10,7 +10,17 @@ return (new LightningConfig())
     ->setReceiver('default-receiver')
     ->setSendableRange(min: 100_000, max: 10_000_000_000)
     ->setCallbackUrl('localhost:8000/callback')
+
+    #################################################
+    # You can add backend configurations in two ways
+    #################################################
+
+    # OPTION A) using an external json
+    ->addBackendsAsJson(__DIR__ . DIRECTORY_SEPARATOR . 'nostr.json')
+
+    # OPTION B) using custom PHP config
     ->setBackends([
         'user-1' => LnBitsBackendConfig::withEndpointAndKey('http://localhost:5000', 'api_key-1'),
         'user-2' => LnBitsBackendConfig::withEndpointAndKey('http://localhost:5000', 'api_key-2'),
-    ]);
+    ])
+;
