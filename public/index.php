739d056595dc90567cdc546e058f42db93bc363a
Chemaclass <chemaclass@outlook.es>
2023-05-14 12:54:45 +0200

    Move Kernel bootstrap method to index.php

--- public/index.php
+++ public/index.php
@@ -2,17 +2,20 @@
 
 declare(strict_types=1);
 
+use Gacela\Framework\Bootstrap\GacelaConfig;
 use Gacela\Framework\Gacela;
 use Gacela\Router\Router;
 use Gacela\Router\Routes;
 use PhpLightning\Invoice\Infrastructure\Controller\InvoiceController;
-use PhpLightning\Kernel;
 
 $cwd = (string)getcwd();
 
 require_once $cwd . '/vendor/autoload.php';
 
-Gacela::bootstrap($cwd, Kernel::gacelaConfigFn());
+Gacela::bootstrap($cwd, static function (GacelaConfig $config): void {
+    $config->addAppConfig('lightning-config.dist.php', 'lightning-config.php');
+    $config->setFileCache(true);
+});
 
 $router = new Router(static function (Routes $routes): void {
     $routes->get('{username}/{amount}', InvoiceController::class);
