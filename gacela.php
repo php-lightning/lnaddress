8c82dd65cbc10c2cb61dc143db50ef7d8a1a292a
Chemaclass <chemaclass@outlook.es>
2023-06-19 18:13:46 +0200

    Add gacela.php to make it optional

---
+++ gacela.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Gacela\Framework\Bootstrap\GacelaConfig;
+use Gacela\Router\Config\RouterGacelaConfig;
+use PhpLightning\Invoice\Infrastructure\Plugin\InvoiceRoutesPlugin;
+
+return static function (GacelaConfig $config) : void {
+    $config
+        ->enableFileCache()
+        ->addAppConfig('lightning-config.dist.php', 'lightning-config.php')
+        ->extendGacelaConfig(RouterGacelaConfig::class)
+        ->addPlugin(InvoiceRoutesPlugin::class);
+};
--- public/index.php
+++ public/index.php
@@ -2,22 +2,17 @@
 
 declare(strict_types=1);
 
-use Gacela\Framework\Bootstrap\GacelaConfig;
 use Gacela\Framework\Gacela;
-use Gacela\Router\Config\RouterGacelaConfig;
 use Gacela\Router\Router;
-use PhpLightning\Invoice\Infrastructure\Plugin\InvoiceRoutesPlugin;
 
 $cwd = (string)getcwd();
 
 require_once $cwd . '/vendor/autoload.php';
 
-Gacela::bootstrap($cwd, static function (GacelaConfig $config): void {
-    $config
-        ->enableFileCache()
-        ->addAppConfig('lightning-config.dist.php', 'lightning-config.php')
-        ->extendGacelaConfig(RouterGacelaConfig::class)
-        ->addPlugin(InvoiceRoutesPlugin::class);
-});
+if (is_file($cwd . '/gacela.php')) {
+    Gacela::bootstrap($cwd);
+} else {
+    Gacela::bootstrap(\dirname(__DIR__));
+}
 
 Gacela::get(Router::class)?->run();
